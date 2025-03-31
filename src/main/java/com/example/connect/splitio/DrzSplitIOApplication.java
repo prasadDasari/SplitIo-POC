d4af3a24997da7bab4caf70b246d8425cadc84fb
PrasadDasari <dvsavprasad@gmail.com>
2025-03-31 21:32:24 +0530

    Update DrzSplitIOApplication.java
    Rename app name

--- src/main/java/com/example/connect/splitio/DrzSplitIOApplication.java
+++ src/main/java/com/example/connect/splitio/DrzSplitIOApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DrzSplitIOApplication {
+public class DemoSplitIOApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DrzSplitIOApplication.class, args);
