b81a17bb325770a6625a16b5714fa1245b9a715f
PrasadDasari <dvsavprasad@gmail.com>
2025-03-31 21:28:23 +0530

    Update FeatureFlagControllerTest.java
    Removed drz references

--- src/test/java/com/example/connect/splitio/FeatureFlagControllerTest.java
+++ src/test/java/com/example/connect/splitio/FeatureFlagControllerTest.java
@@ -31,27 +31,27 @@ public class FeatureFlagControllerTest {
 
         // Test when logging_level_flag is set to DEBUG
         Mockito.when(featureFlagService.getLoggingLevel("user1")).thenReturn("DEBUG");
-        Mockito.when(featureFlagService.isFeatureEnabled("user1", "drz_logging_level_flag")).thenReturn(true);
+        Mockito.when(featureFlagService.isFeatureEnabled("user1", "env_logging_level_flag")).thenReturn(true);
         mockMvc.perform(get("/api/feature")
                         .param("userKey", "user1")
-                        .param("featureName", "drz_logging_level_flag"))
+                        .param("featureName", "env_logging_level_flag"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Feature is ON | Logging level is set to: DEBUG"));
 
         // Test when logging_level_flag is set to INFO
         Mockito.when(featureFlagService.getLoggingLevel("user1")).thenReturn("INFO");
-        Mockito.when(featureFlagService.isFeatureEnabled("user1", "drz_logging_level_flag")).thenReturn(true);
+        Mockito.when(featureFlagService.isFeatureEnabled("user1", "env_logging_level_flag")).thenReturn(true);
         mockMvc.perform(get("/api/feature")
                         .param("userKey", "user1")
-                        .param("featureName", "drz_logging_level_flag"))
+                        .param("featureName", "env_logging_level_flag"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Feature is ON | Logging level is set to: INFO"));
 
-        // Test when the feature flag is off for "drz_logging_level_flag"
-        Mockito.when(featureFlagService.isFeatureEnabled("user1", "drz_logging_level_flag")).thenReturn(false);
+        // Test when the feature flag is off for "env_logging_level_flag"
+        Mockito.when(featureFlagService.isFeatureEnabled("user1", "env_logging_level_flag")).thenReturn(false);
         mockMvc.perform(get("/api/feature")
                         .param("userKey", "user1")
-                        .param("featureName", "drz_logging_level_flag"))
+                        .param("featureName", "env_logging_level_flag"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Feature is OFF"));
     }
